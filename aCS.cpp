600d525e811e08389245e8dfe96b93f8920b74c7
qaptoR <q1pt2rx@users.noreply.github.com>
2019-09-18 02:11:49 -0700

    Further examples of good style

--- aCS.cpp
+++ aCS.cpp
@@ -121,7 +121,7 @@
 [?]     // lastly, always leave one balnk space at the end of a header comment
 [?]         // so that it remains aesthetically pleasing (it just looks wrong otherwise)
 [?]
-  \*- --- --- --- -*\/   // make sure to end this header properly (without \ )
+  \*- --- --- --- -*"/"  // make sure to end this header properly (without \ )
                             // i can't becuase of comment in comment rules
 
     Notice the particular use of whitespace for the opening plate /*- --- ---   Title
@@ -165,7 +165,7 @@
      /*- --- ---    main
     --
     --
-     \*- --- --- --- -*\/
+     \*- --- --- --- -*"/"
 
 //----  --- ----
         int main () {
@@ -175,7 +175,7 @@
     The header, if you can't tell immediately, like all other function headers is indented
         one level to differentiate it from the program header. It's top and bottom plates
         are also only shifted inwards one space instead of two, and the left wall is visually
-        distinct becasuse of the -- used here and nowhere else.
+        distinct becasuse of the -- used for this header and no other.
 
     Above the actual words "int main" the comment dashes (-) hover over each letter to act
         as a sort of attention director to those words. This is done specifically because 
@@ -254,16 +254,32 @@
                 now that you have created the necessary visual space (here 3 blank lines).
             printf("example");
 
-//++++  TODO: FIX THIS!!
-        //  and you can even do a multi-line comment
-        //--  by using -- for each thought attached
+        //  and you can even spread a comment over many lines
+        //--  by using -- for each thought attached to the previous
         //--  so this is still considered the first thought
         //  --  but this would be a sub thought to the first
-        //--  --  which can continue
-        //  --but to start a second sub thought
+        //  ----  which can continue
+        //  ==  but to start a second sub thought 
+        //  ====  (alternating between -/= )
+        //  --  so that a third thought
+        //  ----  is easy to spot
+        //--  and then you can come back to a continuation of the first thought
         //  and this is a second top level thought
+            cout << "example" << endl;
+            
+
             return type;
         }
+
+            The reason that a /* *"/" multi-line comment is not used is because using multiple
+                single line comments is dual purpose: creating bulleted ideas, and expressing
+                the meaning of the code. The other reason is because a multi-line comment
+                is reserved visually for headers; trying to use them elsewhere can easily
+                make code become confusing to follow.
+            Since these comments spread over multiple lines can become just a wall of text
+                it's important to use them sparingly and only when using them is
+                paramount to explaining the function of the code that follows.
+                    it's more of a "just in case" tool for those special circumstances
 
 
             -------------------------------------------------------
@@ -303,10 +319,13 @@
         above is a perfect example of when something needs to be added that
             can be explained in one line (see below for more)
 
+
 //++++  TODO  ++++
+
   //-1  Thing one
     //  still thing one
-    //--  yet still thing one
+    //--  sub thought for thing one
+    //  --  which can continue
   //-2  thing two
   //-3  thing three
   ////  fourth thing (non-specific order)
@@ -318,10 +337,110 @@
             the "trough" with -- draws the eyes into what's happening        
 
 
+        -----------------------------------------------------------------------
+
+    a Special Note on Naming conventions for programs:
+
+        as alluded to earlier by the example program and some previous explanations
+            there are certain naming conventions that "just make sense" to me that I
+            have "figured out" through trial and error. 
+        In particular are the uses of the characters $ and _ which both are normally 
+            recommended to not be used for user defined names for variables or types.
+            I, however, disagree with this advice. Perhaps it was necessary with legacy 
+            code (and may still be), but for modern applications the advent of namespaces
+            has all but iradicated the need for such strict policy-ing. 
+
+        Typically, when creating a user defined type, I choose to make it visually 
+            distinct by beginning with a $ for type definitions, or something like 
+            e$ for an enum type etc.
+        
+    //  for type definitions this essentially looks like:
+        typedef type $newName;
+    //  e.g.
+        typedef const double $cd;
+
+            Notice that the newName should be something easy to recall while also being
+                significantly shorter in order to maximize the point of a typedef.
+                    This reasoning is why $ is used at all for this purpose, in order
+                        to aid in the visual rememberance of what the newName means.
+
+    //  and for enums it looks like:
+        enum e$name { thing=1, other, such, stuff, ... };
+    //  e.g.
+        enum e$months { jan =1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec };
+
+            Notice that unlike type definitions, enumerated types should be named with 
+                clarity in mind, then brevity if possible.
+
+
+        For the use of _ it is important to think of what it SHOULD represent. In my mind
+            it should be used to represent an idea that other characters normally would, 
+            but otherwise can't becuase of variable naming conventions.
+        With that in mind, I believe that one such use is to emulate the use of ( and ), 
+            though, with obvious clear purpose.
+                **** The use of PascalCase and camelCase is good at making different words
+                    in a variable name distinct, therefore using _ to separate words as in
+                    snake_case is a waste of potential for _ ****
+    
+    //  as you can see, for naming mathematical functions e.g. f(x) or A(r)
+    //--  is quite impossible without the use of ( )'s, which is where _ comes in handy
+        double dA_r_ = pi*pow(radius, square);
+        int    iF_x_ = m*x+b;
+
+        
+        Perhaps it's also easy to see from the examples of variable and function names that
+            the first lower case letter represents the data type of that variable or function.
+        This is important because it assists in remembering when it can be used to store data or
+            as a parameter for a function.
+
+        
+        There is no reason (in my mind) to have any special rules for functions() <- because of 
+            exactly what I just wrote. Functions are already denoted by (), therefore it is 
+            unnecessary to create excessive bloat for a functions name() when it's very
+            clearly a function().
+
+    //  See now that the first letter makes it easy to know if an error is being made
+        int    iVar = 9;
+        bool   bVar = false;
+        double dVar = 3.14;
+        char   cVar = "A MISTAKE!";
+        char*  sVar = "s is for string";
+
+    //  don't mind that I've skipped the header!
+    //--  notice that the use of first letters is consistent even within functions that 
+    //--  don't call elsewhere. This is because code may always change, and it's better
+    //--  to do trivial work upfront, rather than menial work later on.
+        void vMessage (int iCookies, bool bJarEmpty) {
+            if (!bJarEmpty) printf("There are %d cookies in the jar\n", iCookies);
+            else           printf("There are no more cookies! Whatever will we do?");
+        }
+
+    //  now when we go to call the function we wrote, we can be sure we're using the
+    //--  correct variables as our parameters (even though the names may be similar)
+        vMessage(iVar, bVar);
+
+            And it's really that simple.
+            I would still make every effort for variable names to be distinct so as to be
+                less confusing, but sometimes it's actually clearer to have two variables
+                with the same name e.g. an integer and a string that represent same data
+
+    //  for a specific number x = 12345
+        int   iX = 12345;
+        char *sX = "12345";
+
+            This can be really useful when trying to recall information that is the same
+                but represented with different data types. Obviously, it's up to you
+                whether or not something more verbose is better
+
+    //  for a specific number y = 678
+        int   integerY = 678;
+        char *stringY  = "678";
+            
+
 //++++  TODO  ++++
-        //Class header
-            //member function header
-        //struct header
-        //enum header
+  ////  Class header
+    //--  member function header
+  ////  struct header
+  ////  enum header
 
 \*--------------------------------------------------------------------------------------------*/
